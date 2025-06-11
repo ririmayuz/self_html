a94b01674c2f59e37e80ff3a3ec4530960d0c11b
Yuz <wwowkdrr@gmail.com>
2025-06-11 10:46:05 +0800

    程式重構與撰寫的概念

---
+++ s20250606/class.php
@@ -0,0 +1,11 @@
+<?php
+// 洗衣服大全class
+
+class 洗衣服大全
+{
+    public function 洗衣服() {}
+
+    public function 烘衣服() {}
+
+    public function 折衣服() {}
+}
---
+++ s20250606/test01.php
@@ -0,0 +1,8 @@
+<?php 
+    include './class.php';
+
+    $obj01 = new 洗衣服大全();
+    $obj01.洗衣服();
+    $obj01.烘衣服();
+    $obj01.折衣服();
+?>
