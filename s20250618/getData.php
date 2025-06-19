95fca936ecbbcc658f416e9e8a95bb479e3b70ad
Yuz <wwowkdrr@gmail.com>
2025-06-19 08:18:03 +0800

    0618

---
+++ s20250618/getData.php
@@ -0,0 +1,29 @@
+<?php
+$input = $_GET;
+$input = $_POST;
+
+$input = [
+    'year' => 10,
+    'money' => 10,
+    'rate' => 8
+];
+
+
+$data = [
+    [
+        'id' => 1,
+        'year' => 1,
+        'money' => 100
+    ]
+];
+
+$dataResult = [];
+// [ [],[],[] ]
+for ($i = 1; $i <= 10; $i++) {
+    $tmpArr =  [
+        'id' => 1,
+        'year' => 1,
+        'money' => 100
+    ];
+    // php array push 
+}
