44d380e528a7aa361a0bc8476eb66553d67ba1d4
Yuz <wwowkdrr@gmail.com>
2025-06-19 09:23:04 +0800

    data跟foreach分頁

---
+++ s20250619/student/data.php
@@ -0,0 +1,24 @@
+<?php
+function dd($data)
+{
+    echo "<pre>";
+    print_r($data);
+    echo "</pre>";
+}
+
+$data = [
+    [
+        'id' => 1,
+        'name' => 'amy',
+    ],
+    [
+        'id' => 2,
+        'name' => 'bob',
+    ],
+    [
+        'id' => 3,
+        'name' => 'cat',
+    ],
+];
+// dd($data);
+?>
--- s20250619/student/index.php
+++ s20250619/student/index.php
@@ -1,26 +1,5 @@
 <?php
-function dd($data)
-{
-    echo "<pre>";
-    print_r($data);
-    echo "</pre>";
-}
-
-$data = [
-    [
-        'id' => 1,
-        'name' => 'amy',
-    ],
-    [
-        'id' => 2,
-        'name' => 'bob',
-    ],
-    [
-        'id' => 3,
-        'name' => 'cat',
-    ],
-];
-// dd($data);
+include "./data.php";
 ?>
 
 <!DOCTYPE html>
