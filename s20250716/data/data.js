e5646f8915c8d817c6387e978c580d18930018c3
Yuz <wwowkdrr@gmail.com>
2025-07-16 16:34:00 +0800

    做卡片

---
+++ s20250716/data/data.js
@@ -0,0 +1,82 @@
+let data = [
+  {
+    "id": 1,
+    "name": "非洲獅",
+    "scientific_name": "Panthera leo",
+    "size_category": "大型",
+    "habitat": "非洲稀樹草原",
+    "conservation_status": "易危 (VU)"
+  },
+  {
+    "id": 2,
+    "name": "孟加拉虎",
+    "scientific_name": "Panthera tigris tigris",
+    "size_category": "大型",
+    "habitat": "印度與孟加拉地區森林",
+    "conservation_status": "瀕危 (EN)"
+  },
+  {
+    "id": 3,
+    "name": "美洲豹",
+    "scientific_name": "Panthera onca",
+    "size_category": "大型",
+    "habitat": "中南美洲雨林",
+    "conservation_status": "近危 (NT)"
+  },
+  {
+    "id": 4,
+    "name": "豹",
+    "scientific_name": "Panthera pardus",
+    "size_category": "大型",
+    "habitat": "非洲與亞洲森林與草原",
+    "conservation_status": "易危 (VU)"
+  },
+  {
+    "id": 5,
+    "name": "雪豹",
+    "scientific_name": "Panthera uncia",
+    "size_category": "中型偏大型",
+    "habitat": "中亞高山地帶",
+    "conservation_status": "易危 (VU)"
+  },
+  {
+    "id": 6,
+    "name": "雲豹",
+    "scientific_name": "Neofelis nebulosa",
+    "size_category": "中型",
+    "habitat": "東南亞熱帶雨林",
+    "conservation_status": "易危 (VU)"
+  },
+  {
+    "id": 7,
+    "name": "猞猁",
+    "scientific_name": "Lynx lynx",
+    "size_category": "中型",
+    "habitat": "歐亞森林",
+    "conservation_status": "無危 (LC)"
+  },
+  {
+    "id": 8,
+    "name": "薮貓",
+    "scientific_name": "Leptailurus serval",
+    "size_category": "中型",
+    "habitat": "非洲濕地與草原",
+    "conservation_status": "無危 (LC)"
+  },
+  {
+    "id": 9,
+    "name": "獰貓",
+    "scientific_name": "Caracal caracal",
+    "size_category": "中型",
+    "habitat": "非洲與西亞沙漠邊緣地區",
+    "conservation_status": "無危 (LC)"
+  },
+  {
+    "id": 10,
+    "name": "家貓",
+    "scientific_name": "Felis catus",
+    "size_category": "小型",
+    "habitat": "全球人類居住區域",
+    "conservation_status": "未列入野生保育分類"
+  }
+]
