f0f6189dca58ff20788a182ef9a65c2a13cc98f6
Yuz <wwowkdrr@gmail.com>
2025-07-30 16:30:19 +0800

    js and jq2

---
+++ s20250730_jq/data.php
@@ -0,0 +1,30 @@
+<?php
+$data = [
+    [
+        'id' => 1,
+        'name' => 'kai',
+        'color' => 'blue',
+        'price' => '1000'
+    ],
+    [
+        'id' => 2,
+        'name' => 'bob',
+        'color' => 'red'
+    ],
+    [
+        'id' => 3,
+        'name' => 'cat',
+        'color' => 'green'
+
+    ]
+];
+
+
+?>
+<button type="button" data-name="kai" data-color="#blue">blueBtn</button>
+<button type="button" data-name="bob" data-color="#red">redBtn</button>
+<button type="button" data-name="cat" data-color="#green">greenBtn</button>
+
+<?php foreach($data as $item) :?>
+<button type="button" data-name="<?= $item['name'] ?>" data-color="<?= $item['color'] ?>"><?= $item['color'] ?>Btn</button>
+<?php endforeach;?>
